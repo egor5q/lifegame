09cdc0470048e6b92a504c2b9e7d19126ea50158
egor5q <31898567+egor5q@users.noreply.github.com>
2019-05-13 15:26:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,7 +120,9 @@ def creategame(chatid, size='77', speed=1):   # x = size[0];  y = size[1];   spe
            }
 
     
-    
+def medit(message_text,chat_id, message_id,reply_markup=None,parse_mode=None):
+    return bot.edit_message_text(chat_id=chat_id,message_id=message_id,text=message_text,reply_markup=reply_markup,
+                                 parse_mode=parse_mode)    
     
     
 print('7777')
